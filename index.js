7b37f099ab0b69c27b38a2590ca9d7c1d3963c68
vijay25890 <vijaymore25890@gmail.com>
2021-09-03 19:48:11 +0530

    datatypes

--- index.js
+++ index.js
@@ -3,4 +3,17 @@
 var myName = "vijay more";
 var myAge = 22;
 console.log(myName, myAge );
-console.log(myName, myAge );
+
+//Datatypes in javascript
+
+var myName = "vijay more";
+var myAge = 22;
+var iAmVijay = true;
+console.log(typeof iAmVijay);
+console.log(typeof myAge);
+console.log(myName);
+console.log(typeof myName);
+console.log(10 - "20");
+console.log("  " + "6 ");
+console.log("vijay" - "more");
+console.log(true);
