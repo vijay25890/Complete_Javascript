b0139f6a8c9f85503c35eb95498303f97b47298e
vijay25890 <vijaymore25890@gmail.com>
2021-09-05 17:32:45 +0530

    add

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Variables in javascript
+
 var myName = "vijay more";
 var myAge = 22;
 console.log(myName, myAge);
 
 //Datatypes in javascript
+
 var myName = "vijay more";
 var myAge = 22;
 var iAmVijay = true;
@@ -17,10 +19,12 @@ console.log("vijay" - "more");
 console.log(true);
 
 //null vs undefined
+
 var mm = null;
 console.log(mm);
 var nn = console.log(nn);
 
 //isNaN 
+
 var MyNum = 5421325465;
 console.log(isNaN(MyNum));
