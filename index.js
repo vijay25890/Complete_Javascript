cd5880c375a0ee68424677d791cb5ee6350b5218
vijay25890 <vijaymore25890@gmail.com>
2021-09-22 20:32:54 +0530

    incr decr

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // Variables in javascript
-
 var myName = "vijay more";
 var myAge = 22;
 console.log(myName, myAge);
 
 //Datatypes in javascript
-
 var myName = "vijay more";
 var myAge = 22;
 var iAmVijay = true;
@@ -19,26 +17,41 @@ console.log("vijay" - "more");
 console.log(true);
 
 //null vs undefined
-
 var mm = null;
 console.log(mm);
 var nn = console.log(nn);
 
 //isNaN
-
 var MyNum = 5421325465;
 console.log(isNaN(MyNum));
 
 //assignment operator
-
 var x = 5;
 var y = 5;
 console.log(`x is equal to y : ${x == y}`);
 
 //arithmatic operator
-
 console.log(20 / 5);
 console.log(20 % 5);
 
-//incr/decr operator
-
+//incr and decr operator
+var num = 15;
+var newNum = num++;
+console.log(num);
+console.log(newNum);
+//output:16 15
+var num = 15;
+var newNum = ++num;
+console.log(num);
+console.log(newNum);
+//output:16 16
+var num = 15;
+var newNum = num--;
+console.log(num);
+console.log(newNum);
+//output:14 15
+var num = 15;
+var newNum = --num;
+console.log(num);
+console.log(newNum);
+//output:14 14 
